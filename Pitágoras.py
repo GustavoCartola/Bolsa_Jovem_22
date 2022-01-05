4ba73ca1f1dfca5412b1d1dd1bc7756b6d0c90ef
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2022-01-04 23:09:04 -0300

    Add files via upload

---
+++ Pitágoras.py
@@ -0,0 +1,4 @@
+catetooposto = float(input("Cateto Oposto: "))
+catetoadjacente = float(input("Cateto Adjacente: "))
+hipotenusa = (catetoadjacente**2 + catetooposto**2)**(1/2)
+print("Hipotenusa = ",(hipotenusa))
