e342b6743731149474c9a6a549a6cae59e538dca
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2024-09-21 18:52:36 -0300

    Update Captcha.py

--- Captcha.py
+++ Captcha.py
@@ -1,5 +1,5 @@
 from captcha.image import ImageCaptcha
-imagem = ImageCaptcha(width=280, height=90)
-texto = input("Digite seu captcha:")
-gerar = imagem.generate(texto)
-imagem.write(texto, 'captcha.png')
+image = ImageCaptcha(width=280, height=90)
+text = input("Digite seu captcha:")  # prompt for captcha input
+generate = image.generate(text)  # generate the captcha image
+image.write(text, 'captcha.png')  # save the captcha image as 'captcha.png'
