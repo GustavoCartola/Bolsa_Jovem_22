68f13710bca035fb55ffb2772277d4f0caa09ed8
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2024-09-21 18:53:36 -0300

    Update and rename Bipe.py to Beep.py

---
+++ Beep.py
@@ -0,0 +1,4 @@
+import winsound
+duration = 500  # duration of the beep
+frequency = 2500  # frequency of the beep
+winsound.Beep(frequency, duration)
--- Bipe.py
+++
@@ -1,4 +0,0 @@
-import winsound
-duration = 500  # duração do beep
-frequency = 2500  # frequência do beep
-winsound.Beep(frequency, duration)
