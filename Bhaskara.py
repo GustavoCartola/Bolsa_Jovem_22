9e8619f8104a45ae6710965021292f2bf1d87887
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2024-09-21 18:53:06 -0300

    Update Bhaskara.py

--- Bhaskara.py
+++ Bhaskara.py
@@ -1,9 +1,9 @@
 avariable = float(input("Valor de A: "))
 bvariable = float(input("Valor de B: "))
 cvariable = float(input("Valor de C: "))
-delta = ((bvariable**2) - (4*avariable*cvariable))  # Calcula o delta
-x1 = ((-bvariable + delta ** (1/2)) / (2*avariable))  # Calcula x1
-x2 = ((-bvariable - delta ** (1/2)) / (2*avariable))  # Calcula x2
+delta = ((bvariable**2) - (4*avariable*cvariable))  # calculate delta
+x1 = ((-bvariable + delta ** (1/2)) / (2*avariable))  # calculate x1
+x2 = ((-bvariable - delta ** (1/2)) / (2*avariable))  # calculate x2
 if delta < 0:
     print('Delta Inválido')
 if delta >= 0:
