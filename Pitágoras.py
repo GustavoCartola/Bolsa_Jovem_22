5dd217b7c257604f3489ae7a7f488e5645202b3d
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2024-09-22 01:11:16 -0300

    Update Pitágoras.py

--- Pitágoras.py
+++ Pitágoras.py
@@ -1,4 +1,4 @@
-catetooposto = float(input("Cateto Oposto: "))
-catetoadjacente = float(input("Cateto Adjacente: "))
-hipotenusa = (catetoadjacente**2 + catetooposto**2)**(1/2)
-print("Hipotenusa = ",(hipotenusa))
+opposite_side = float(input("Cateto Oposto: "))  # Prompt for the opposite side
+adjacent_side = float(input("Cateto Adjacente: "))  # Prompt for the adjacent side
+hypotenuse = (adjacent_side**2 + opposite_side**2)**(1/2)  # Calculate the hypotenuse
+print("Hipotenusa = ", hypotenuse)  # Print the hypotenuse
