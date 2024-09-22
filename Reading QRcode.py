1eee5cb3eae1110c8341a6a4cd30160150290838
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2024-09-22 01:02:50 -0300

    Update and rename Lendo QRcode.py to Reading QRcode.py

--- Reading QRcode.py
+++ Reading QRcode.py
@@ -1,5 +1,8 @@
 from PIL import Image
 from pyzbar.pyzbar import decode
 
+# Decode the QR code from the image
 read = decode(Image.open('qrcode.png'))
+
+# Print the decoded data from the QR code
 print(read[0].data)
