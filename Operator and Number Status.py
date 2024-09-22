482ffc5833d59fe45528bcfd3ceb4d44064e1763
Gustavo da Silva <83617289+GustavoCartola@users.noreply.github.com>
2024-09-22 01:11:53 -0300

    Update Operator and Number Status.py

--- Operator and Number Status.py
+++ Operator and Number Status.py
@@ -1,10 +1,10 @@
 import phonenumbers
 from phonenumbers import geocoder, carrier
 
-numero = input('Número: ')  # Prompt for the phone number
-celular = phonenumbers.parse(numero)  # Parse the phone number
-operadora = carrier.name_for_number(celular, 'pt-br')  # Get the carrier name in Portuguese
-estado = geocoder.description_for_number(celular, 'pt-br')  # Get the state description in Portuguese
+number = input('Número: ')  # Prompt for the phone number
+cellphone = phonenumbers.parse(number)  # Parse the phone number
+carrier_name = carrier.name_for_number(cellphone, 'pt-br')  # Get the carrier name in Portuguese
+state = geocoder.description_for_number(cellphone, 'pt-br')  # Get the state description in Portuguese
 
-print("A operadora é: " + operadora)  # Print the carrier
-print("O estado é: " + estado)  # Print the state
+print("A operadora é: " + carrier_name)  # Print the carrier
+print("O estado é: " + state)  # Print the state
